a5566f6fb429404ed2ab853dfd978e21b29ac799
Ashish Patil <ashishsanjaypatil@gmail.com>
2021-04-27 20:18:27 +0530

    Snapping defenders to grids

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -7,15 +7,24 @@ public class DefenderSpawner : MonoBehaviour
     [SerializeField]
     GameObject defenderPrefab;
 
+    LevelLoader levelLoading;
+
+    private void Start()
+    {
+        levelLoading = FindObjectOfType<LevelLoader>();
+    }
+
     private void OnMouseDown()
     {
-        Instantiate(defenderPrefab, GetSquareClicked(), Quaternion.identity);
+        GameObject newCactus = Instantiate(defenderPrefab, GetSquareClicked(), Quaternion.identity);
+        newCactus.transform.parent = levelLoading.GetCleanUpContainer();
     }
 
     private Vector2 GetSquareClicked()
     {
         Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
-        return worldPos;
+        Vector2 gridPos = new Vector2(Mathf.RoundToInt(worldPos.x), Mathf.RoundToInt(worldPos.y));
+        return gridPos;
     }
 }
