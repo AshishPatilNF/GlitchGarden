403c71ad5e7c86a9cc0a9c99cc84f6310fbe2f35
Ashish Patil <ashishsanjaypatil@gmail.com>
2021-04-29 20:31:30 +0530

    Instantiating Attackers as children of spawners

--- Assets/Scripts/AttackerSpawner.cs
+++ Assets/Scripts/AttackerSpawner.cs
@@ -7,14 +7,11 @@ public class AttackerSpawner : MonoBehaviour
     [SerializeField]
     GameObject attackerPrefab;
 
-    private LevelLoader levelLoading;
-
     bool spawn = true;
 
     // Start is called before the first frame update
     IEnumerator Start()
     {
-        levelLoading = FindObjectOfType<LevelLoader>();
         while(spawn)
         {
             yield return new WaitForSeconds(Random.Range(1, 5));
@@ -31,6 +28,6 @@ public class AttackerSpawner : MonoBehaviour
     private void SpawnAttacker()
     {
         GameObject newAttacker = Instantiate(attackerPrefab, transform.position, Quaternion.identity);
-        newAttacker.transform.parent = levelLoading.GetCleanUpContainer();
+        newAttacker.transform.parent = transform;
     }
 }
