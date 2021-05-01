86cbbd683d21a4ad1d2b6b2d227b506eba84037e
Ashish Patil <ashishsanjaypatil@gmail.com>
2021-05-01 22:00:47 +0530

    New Defender Laner Gnome Added

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DamageDealer : MonoBehaviour
 {
     [SerializeField]
-    int damage = 10; 
+    int damage = 10;
 
     // Start is called before the first frame update
     void Start()
